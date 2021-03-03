7f372e1b5c0acaa37168829cdfe771090076f749
CharKourkoulis <cult_bob@hotmail.com>
2021-03-03 22:40:59 +0200

    Relationships - Fluent API

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -12,12 +12,26 @@ namespace SamuraiApp.Data
     {
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<Horse> Horses { get; set; }
+
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=SamuraiAppData");
             
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            modelbuilder.Entity<Samurai>()
+                .HasMany(s => s.Battles)
+                .WithMany(b => b.Samurais)
+                .UsingEntity<BattleSamurai>(bs => bs.HasOne<Battle>().WithMany(),
+                                            bs => bs.HasOne<Samurai>().WithMany())
+                .Property(bs => bs.DateJoined)
+                .HasDefaultValueSql("getDate()");
         }
     }
 }
